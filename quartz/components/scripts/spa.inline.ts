f9e797bc251f34981e2471e07b3ad748ff5038cd
JayClock <35857909+JayClock@users.noreply.github.com>
2024-03-02 23:22:20 +0800

    feat: add comment

--- quartz/components/scripts/spa.inline.ts
+++ quartz/components/scripts/spa.inline.ts
@@ -1,5 +1,15 @@
 import micromorph from "micromorph"
 import { FullSlug, RelativeURL, getFullSlug, normalizeRelativeURLs } from "../../util/path"
+import { init } from "@waline/client"
+
+document.addEventListener("nav", () => {
+  const walineElement = document.getElementById("waline")
+  const walineInstance = init({
+    el: walineElement,
+    serverURL: "https://comment.jayclock-garden.top",
+  })
+  window.addCleanup(() => walineInstance?.destroy())
+})
 
 // adapted from `micromorph`
 // https://github.com/natemoo-re/micromorph
