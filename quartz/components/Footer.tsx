dff050dee54bc23806e789356842f59e859e568e
JayClock <35857909+JayClock@users.noreply.github.com>
2024-03-02 23:22:20 +0800

    feat: add comment

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -14,6 +14,8 @@ export default ((opts?: Options) => {
     return (
       <footer class={`${displayClass ?? ""}`}>
         <hr />
+        <div id="waline" spa-preserve="true">waline</div>
+        <hr />
         <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
           <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
