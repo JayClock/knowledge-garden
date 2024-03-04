c049d80085bead08bafc32b411651d4105047dbb
JayClock <35857909+JayClock@users.noreply.github.com>
2024-03-04 23:12:07 +0800

    fix: remove useless code

--- quartz/components/renderPage.tsx
+++ quartz/components/renderPage.tsx
@@ -2,7 +2,6 @@ import { render } from "preact-render-to-string"
 import { QuartzComponent, QuartzComponentProps } from "./types"
 import HeaderConstructor from "./Header"
 import BodyConstructor from "./Body"
-import CommentConstructor from "./Comment"
 import { JSResourceToScriptElement, StaticResources } from "../util/resources"
 import { clone, FullSlug, RelativeURL, joinSegments, normalizeHastElement } from "../util/path"
 import { visit } from "unist-util-visit"
@@ -194,7 +193,6 @@ export function renderPage(
   } = components
   const Header = HeaderConstructor()
   const Body = BodyConstructor()
-  const Comment = CommentConstructor()
 
   const LeftComponent = (
     <div class="left sidebar">
@@ -237,8 +235,6 @@ export function renderPage(
             </div>
             {RightComponent}
           </Body>
-          <Comment {...componentData} />
-          <hr />
           <Footer {...componentData} />
         </div>
       </body>
