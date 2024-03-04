37d25bc0d603d31276141c114569cb8277777836
JayClock <35857909+JayClock@users.noreply.github.com>
2024-03-04 18:31:53 +0800

    feat: add Comment

--- quartz/components/renderPage.tsx
+++ quartz/components/renderPage.tsx
@@ -2,6 +2,7 @@ import { render } from "preact-render-to-string"
 import { QuartzComponent, QuartzComponentProps } from "./types"
 import HeaderConstructor from "./Header"
 import BodyConstructor from "./Body"
+import CommentConstructor from "./Comment"
 import { JSResourceToScriptElement, StaticResources } from "../util/resources"
 import { clone, FullSlug, RelativeURL, joinSegments, normalizeHastElement } from "../util/path"
 import { visit } from "unist-util-visit"
@@ -186,6 +187,7 @@ export function renderPage(
   } = components
   const Header = HeaderConstructor()
   const Body = BodyConstructor()
+  const Comment = CommentConstructor()
 
   const LeftComponent = (
     <div class="left sidebar">
@@ -228,6 +230,8 @@ export function renderPage(
             </div>
             {RightComponent}
           </Body>
+          <Comment {...componentData} />
+          <hr />
           <Footer {...componentData} />
         </div>
       </body>
