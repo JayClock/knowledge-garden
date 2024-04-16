51da64a6c7cabf2832495c268e2c8a5be328e66a
JayClock <35857909+JayClock@users.noreply.github.com>
2024-04-16 11:06:03 +0800

    fix: change defaultDateType

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     locale: "zh-CN",
     baseUrl: "www.jayclock-garden.top",
     ignorePatterns: ["private", "templates", ".obsidian"],
-    defaultDateType: "created",
+    defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
