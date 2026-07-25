7052a5d70a07110d25b520e8465477d8cc3b30f4
JayClock <35857909+JayClock@users.noreply.github.com>
2026-07-25 11:18:05 +0800

    fix: hide published assets from explorer

--- quartz.ts
+++ quartz.ts
@@ -1,4 +1,9 @@
+import * as ExternalPlugin from "./.quartz/plugins"
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+
+ExternalPlugin.Explorer({
+  filterFn: (node) => !["tags", "assets"].includes(node.slugSegment ?? ""),
+} satisfies Partial<ExternalPlugin.ExplorerOptions>)
 
 const config = await loadQuartzConfig()
 export default config
